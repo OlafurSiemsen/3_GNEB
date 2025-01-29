63c9436b8ef28c1d41cc71f57feacc2bd724c885
Olafur Siemsen <OlafurSiemsen@ProtonMail.com>
2025-01-29 10:29:14 +0100

    demoing a bug in the subslice implementation

--- engine/run.go
+++ engine/run.go
@@ -174,9 +174,9 @@ func RunWhile(condition func() bool) {
 	pause = false // may be set by <-Inject
 	const output = true
 	stored_buffer_ptr := M.buffer_
-	for i_image := range 1 { //M.buffer_.N_images {
-		i_image = i_image
-		// M.buffer_ = stored_buffer_ptr.SubSlice(i_image)
+	for i_image := range M.buffer_.N_images {
+		// i_image = i_image
+		M.buffer_ = stored_buffer_ptr.SubSlice(i_image)
 		pause = false  // may be set by <-Inject
 		stepper.Free() // start from a clean state
 		runWhile(condition, output)
