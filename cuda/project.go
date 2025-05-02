c0cd23c7eefc5bba3cb5abdcb6edf4a3dabe28f6
Olafur Siemsen <OlafurSiemsen@ProtonMail.com>
2025-05-02 12:36:26 +0200

    Implemented functions for projections onto space and tangent space

--- cuda/project.go
+++ cuda/project.go
@@ -6,6 +6,7 @@ import (
 )
 
 // Projects a onto the tangent space of b
+// Assumes vectors in b are normalized
 // dst[ith cell] = a[ith cell] - (dot(a[ith cell],b[ith cell]) * b[ith cell]
 func ProjectOntoTangent(dst, a, b *data.Slice) {
 	util.Argument(dst.NComp() == 3 && a.NComp() == 3 && b.NComp() == 3)
@@ -14,6 +15,21 @@ func ProjectOntoTangent(dst, a, b *data.Slice) {
 	N := dst.Len()
 	cfg := make1DConf(N)
 	k_projectontotangent_async(dst.DevPtr(X), dst.DevPtr(Y), dst.DevPtr(Z),
+		a.DevPtr(X), a.DevPtr(Y), a.DevPtr(Z),
+		b.DevPtr(X), b.DevPtr(Y), b.DevPtr(Z),
+		N, cfg)
+}
+
+// Projects a onto b
+// Assumes vectors in b are normalized
+// dst[ith cell] = (dot(a[ith cell],b[ith cell]) * b[ith cell]
+func ProjectOnto(dst, a, b *data.Slice) {
+	util.Argument(dst.NComp() == 3 && a.NComp() == 3 && b.NComp() == 3)
+	util.Argument(dst.Len() == a.Len() && dst.Len() == b.Len())
+
+	N := dst.Len()
+	cfg := make1DConf(N)
+	k_projectonto_async(dst.DevPtr(X), dst.DevPtr(Y), dst.DevPtr(Z),
 		a.DevPtr(X), a.DevPtr(Y), a.DevPtr(Z),
 		b.DevPtr(X), b.DevPtr(Y), b.DevPtr(Z),
 		N, cfg)
